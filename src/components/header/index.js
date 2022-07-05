a57bd799ca60147eec8a4153ab26e3dd73445a9b
Tyler <Adonis11300522@gmail.com>
2022-07-05 06:00:19 -0300

    commit

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -5,6 +5,7 @@ import { FaApple, FaFacebookF, FaGoogle } from "react-icons/fa";
 
 function Header () {
     const [inShow, setInShow] = useState(false);
+    const [upShow, setUpShow] = useState(false);
     const [validated, setValidated] = useState(false);
 
     const handleSubmit = (event) => {
@@ -31,7 +32,7 @@ function Header () {
                     </Nav>
                     <Nav>
                         <Nav.Link href="" className="px-2 mx-3 text-center" onClick={() => setInShow(true)}>Sign In</Nav.Link>
-                        <Nav.Link href="" className="px-2 mx-3 text-center">Sign Up</Nav.Link>
+                        <Nav.Link href="" className="px-2 mx-3 text-center" onClick={() => setUpShow(true)}>Sign Up</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
                 </Container>
@@ -72,6 +73,60 @@ function Header () {
                                         </Form.Group>
                                         <Button type="submit">Sign In</Button>
                                         <div className="text-center my-3">or Sign In with</div>
+                                        <Button className="mb-3 align-items-center d-flex justify-content-center bg-danger signin-btn" type="submit"><FaGoogle className="mx-3"/>Google</Button>
+                                        <Button className="mb-3 align-items-center d-flex justify-content-center bg-info signin-btn" type="submit"><FaFacebookF className="mx-3"/>Facebook</Button>
+                                        <Button className="align-items-center d-flex justify-content-center bg-dark signin-btn" type="submit"><FaApple className="mx-3"/>Apple</Button>
+                                        <div className="text-center my-3">Don't have an account? <Nav.Link onClick={() =>{ setUpShow(true); setInShow(false)}}>Sign Up</Nav.Link></div>
+                                    </Row>
+                                </Form>
+                            </Col>
+                            <Col lg={6} md={6}>
+                                <Image src="/assets/images/banner/1.jpg" height="100%" width="100%"/>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Modal.Body>
+            </Modal>
+            <Modal
+                size="lg"
+                show={upShow}
+                onHide={() => setUpShow(false)}
+                aria-labelledby="example-modal-sizes-title-sm"
+                centered
+            >                
+                <Modal.Body className="overflow-hidden">
+                    <Container>
+                        <Row>
+                            <Col lg={6} md={6} sm={12}>
+                                <div className="logo-image text-center">
+                                    <Image src="/assets/images/logo.png" width="100"/>
+                                    <h4>Welcome ScanNGet!</h4>
+                                </div>
+                                <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                                    <Row className="mb-3">
+                                        <Form.Group controlId="validationCustom01" className="mb-3">
+                                            <Form.Label>User Name</Form.Label>
+                                            <Form.Control required type="email" placeholder="User Name"/>
+                                            <Form.Control.Feedback type="invalid">Please input a valid User Name!</Form.Control.Feedback>
+                                        </Form.Group>
+                                        <Form.Group controlId="validationCustom02"  className="mb-3">
+                                            <Form.Label>Password</Form.Label>
+                                            <Form.Control required type="password" placeholder="Password"/>
+                                            <Form.Control.Feedback type="invalid">Please input a valid Password!</Form.Control.Feedback>
+                                        </Form.Group>
+                                        <Form.Group controlId="validationCustom03"  className="mb-3">
+                                            <Form.Label>Confirm Password</Form.Label>
+                                            <Form.Control required type="password" placeholder="Password"/>
+                                            <Form.Control.Feedback type="invalid">Please input a valid Password!</Form.Control.Feedback>
+                                        </Form.Group>
+                                        <Form.Group className="mb-3 d-flex justify-content-between align-items-center">
+                                        <Form.Check
+                                            required
+                                            label="Remember Me!"
+                                            />
+                                        </Form.Group>
+                                        <Button type="submit">Sign Up</Button>
+                                        <div className="text-center my-3">or Sign Up with</div>
                                         <Button className="mb-3 align-items-center d-flex justify-content-center bg-danger signin-btn" type="submit"><FaGoogle className="mx-3"/>Google</Button>
                                         <Button className="mb-3 align-items-center d-flex justify-content-center bg-info signin-btn" type="submit"><FaFacebookF className="mx-3"/>Facebook</Button>
                                         <Button className="align-items-center d-flex justify-content-center bg-dark signin-btn" type="submit"><FaApple className="mx-3"/>Apple</Button>
