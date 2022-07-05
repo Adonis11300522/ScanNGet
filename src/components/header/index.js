89bfeecb2d2a5343f0945fc67175902133be49f5
Tyler <Adonis11300522@gmail.com>
2022-07-05 05:48:02 -0300

    commit

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import {Navbar, Nav, Container, Image, Modal, Form, Button, Row, Col} from 'react-bootstrap';
 import Logo from '../../assets/images/logo.png';
+import { FaApple, FaFacebookF, FaGoogle } from "react-icons/fa";
 
 function Header () {
     const [inShow, setInShow] = useState(false);
@@ -19,7 +20,7 @@ function Header () {
         <div className="Header">
             <Navbar collapseOnSelect expand="lg" className="py-3" fixed="top">
                 <Container className="rounded">
-                <Navbar.Brand to="/"><Image src={Logo} width="50" className="my-2"/></Navbar.Brand>
+                <Navbar.Brand href="/"><Image src={Logo} width="50" className="my-2"/></Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="mx-auto">
@@ -71,14 +72,14 @@ function Header () {
                                         </Form.Group>
                                         <Button type="submit">Sign In</Button>
                                         <div className="text-center my-3">or Sign In with</div>
-                                        <Button className="mb-3" type="submit">Sign In</Button>
-                                        <Button className="mb-3" type="submit">Sign In</Button>
-                                        <Button className="mb-3" type="submit">Sign In</Button>
+                                        <Button className="mb-3 align-items-center d-flex justify-content-center bg-danger signin-btn" type="submit"><FaGoogle className="mx-3"/>Google</Button>
+                                        <Button className="mb-3 align-items-center d-flex justify-content-center bg-info signin-btn" type="submit"><FaFacebookF className="mx-3"/>Facebook</Button>
+                                        <Button className="align-items-center d-flex justify-content-center bg-dark signin-btn" type="submit"><FaApple className="mx-3"/>Apple</Button>
                                     </Row>
                                 </Form>
                             </Col>
                             <Col lg={6} md={6}>
-                                <Image src="/assets/images/banner/3.webp"/>
+                                <Image src="/assets/images/banner/1.jpg" height="100%" width="100%"/>
                             </Col>
                         </Row>
                     </Container>
